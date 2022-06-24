5dcecbef3ed3e1de0d2bb19d17c899c04de30716
LeeVaughn <dlvprogramming@gmail.com>
2022-06-24 16:28:45 -0700

    Add code to complete 7 of 10 tests

--- time_calculator.py
+++ time_calculator.py
@@ -28,25 +28,42 @@ def add_time(start, duration, day=None):
         new_minutes = str(new_minutes)
 
     days = total_hours // 24
-    
 
-    if total_hours <= 12:
-        new_time = str(total_hours) + ':' + new_minutes + ' AM'
-    elif total_hours <= 24:
-        new_time = str(total_hours - 12) + ':' + new_minutes + ' PM'
+    if total_hours > 24:
+        new_hours = total_hours - (days * 24)
+    else:
+        new_hours = total_hours
+    
+    if new_hours < 12:
+        new_time = str(new_hours) + ':' + new_minutes + ' AM'
+
+        if new_hours == 0:
+            new_time = '12:' + new_minutes + ' AM'
+    elif new_hours < 24:
+        new_time = str(new_hours - 12) + ':' + new_minutes + ' PM'
+
+        if new_hours == 12:
+            new_time = '12:' + new_minutes + ' PM'
 
     if day != None:
         new_time = new_time + ', ' + day
-
-    # print(hour)
-    # print(new_minutes)
-    # print(days)
-
-
-
-
+    elif days == 1:
+        new_time = new_time + ' (next day)'
+    elif days >= 2:
+        new_time = new_time + f' ({days} days later)'
 
     print(new_time)
     return new_time
 
-add_time("11:43 AM", "00:20", "Monday")
+add_time("3:00 PM", "3:10")
+# Returns: 6:10 PM
+add_time("11:30 AM", "2:32", "Monday")
+# Returns: 2:02 PM, Monday
+add_time("11:43 AM", "00:20")
+# Returns: 12:03 PM
+add_time("10:10 PM", "3:30")
+# Returns: 1:40 AM (next day)
+add_time("11:43 PM", "24:20", "tueSday")
+# Returns: 12:03 AM, Thursday (2 days later)
+add_time("6:30 PM", "205:12")
+# Returns: 7:42 AM (9 days later)
