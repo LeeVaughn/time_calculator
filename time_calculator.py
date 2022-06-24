6da1d1aa40847b02ce9a7cdff55dfc84e47d2b24
LeeVaughn <dlvprogramming@gmail.com>
2022-06-24 14:29:58 -0700

    Add code to calcuate time for same day

--- time_calculator.py
+++ time_calculator.py
@@ -1,27 +1,49 @@
-def add_time(start, duration):
+def add_time(start, duration, day=None):
     start_split = start.split(' ')
     am_pm = start_split[1]
-    hours = int(start_split[0].split(':')[0])
+    hour = int(start_split[0].split(':')[0])
     minutes = int(start_split[0].split(':')[1])
-    add_hours = int(duration.split(':')[0])
-    add_minutes = int(duration.split(':')[1])
-    days = 0
+    hours_to_add = int(duration.split(':')[0])
+    minutes_to_add = int(duration.split(':')[1])
+    new_minutes = 0
+    new_hours = 0
+    new_time = None
 
+    # convert time to military equivalent
     if am_pm == 'PM':
-        hours += 12
+        hour += 12
 
-    total_hours = hours + add_hours
+    total_hours = hour + hours_to_add
+    total_minutes = minutes + minutes_to_add
+
+    if total_minutes > 60:
+        total_hours += 1
+        new_minutes = total_minutes - 60
+    else:
+        new_minutes = total_minutes
+
+    if new_minutes < 10:
+        new_minutes = '0' + str(new_minutes)
+    else:
+        new_minutes = str(new_minutes)
+
+    days = total_hours // 24
     
-    if total_hours > 24:
-        days = total_hours // 24
 
-    print(days)
+    if total_hours <= 12:
+        new_time = str(total_hours) + ':' + new_minutes + ' AM'
+    elif total_hours <= 24:
+        new_time = str(total_hours - 12) + ':' + new_minutes + ' PM'
+
+    # print(hour)
+    # print(new_minutes)
+    # print(days)
 
 
 
 
 
+    print(new_time)
+    return new_time
 
-    # return new_time
-
-add_time("3:00 PM", "27:10")
+add_time("11:43 AM", "00:20")
